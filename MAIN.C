e206d986a8f3171094268b6c13527f315d0eb4b5
Heli Villarreal <acmzero@gmail.com>
2017-10-08 22:44:09 -0500

    Estructura de proyecto

---
+++ MAIN.C
@@ -0,0 +1,8 @@
+#include "include/procesos.h"
+
+
+int main(void){
+  proceso_2();
+  return 0;
+}
+
---
+++ include/procesos.h
@@ -0,0 +1,16 @@
+/**
+ * Cabecera de los 6 procesos a ejecutar
+ * 1) Iniciar pantalla y los demas procesos
+ * 2) Mostrar pelota(s) rebotando en las esquinas
+ * 3) Mostrar fecha/hora 
+ * 4) Imprimir un mensaje tomado de un array
+ * 5) Editor de texto
+ * 6) Dummy (while true)
+ */
+
+void proceso_1();
+void proceso_2();
+void proceso_3();
+void proceso_4();
+void proceso_5();
+void proceso_6();
